4b1f9f890fc7bd09beb0640bd4cf36a5af504e7d
HouBinyang <21302010042@m.fudan.edu.cn>
2024-01-24 03:17:36 +0800

    simple tile_map

--- Scripts/Player/PlayerBaseState/PlayerWallSlideState.cs
+++ Scripts/Player/PlayerBaseState/PlayerWallSlideState.cs
@@ -17,19 +17,27 @@ public class PlayerWallSlideState : PlayerState
     {
         base.Update();
         
-        player.SetVelocity(0,rb.velocity.y*0.3f);
+        if(rb.velocity.y<0)
+            player.SetVelocity(0,rb.velocity.y*0.3f);
 
 
         if (Input.GetKeyDown(KeyCode.Space) && player.IsWallDetected())
         {
-            if(xInput>0 && player.facingDir<0 || xInput<0 && player.facingDir>0)
+            if (xInput > 0 && player.facingDir < 0 || xInput < 0 && player.facingDir > 0)
+            {
                 player.SetVelocity(player.jumpForce/10 * player.facingDir*(-1f),rb.velocity.y);
-            
-            playerStateMachine.ChangeState(player.jumpState);
+                playerStateMachine.ChangeState(player.jumpState);
+            }
+            else if (xInput > 0 && player.facingDir > 0 || xInput < 0 && player.facingDir < 0 || xInput == 0)
+            {
+                rb.velocity = new Vector2(0, player.jumpForce);
+            }
         }
         
         if(player.IsGroundDetected())
             playerStateMachine.ChangeState(player.idleState);
+        if(!player.IsWallDetected())
+            playerStateMachine.ChangeState(player.airState);
     }
 
     public override void Exit()
