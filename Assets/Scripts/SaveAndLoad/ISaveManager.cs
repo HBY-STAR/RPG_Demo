1cdc063128ca1b5c516a6b0e172615aa8c1d03e1
HouBinyang <21302010042@m.fudan.edu.cn>
2024-06-30 18:02:53 +0800

    Remove squashed Assets subtree

--- Assets/Scripts/SaveAndLoad/ISaveManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ISaveManager
-{
-    void LoadData(GameData gameData);
-    void SaveData(ref GameData gameData);
-}
